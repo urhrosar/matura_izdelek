498d3e1ea7dc8cd158904bce59c1b6715526a26c
UrhSlo <59819345+UrhSlo@users.noreply.github.com>
2022-05-03 13:58:50 +0200

    graf popravljen

--- script.js
+++ script.js
@@ -185,11 +185,67 @@ input.addEventListener('change', () => {
                 chartArea.right - chartArea.left, chartArea.top - chartArea.bottom);
             }
           };
+
+          var horizonalLinePlugin = {
+            afterDraw: function(chartInstance) {
+              var yScale = chartInstance.scales["y-axis-0"];
+              var canvas = chartInstance.chart;
+              var ctx = canvas.ctx;
+              var index;
+              var line;
+              var style;
+          
+              if (chartInstance.options.horizontalLine) {
+                for (index = 0; index < chartInstance.options.horizontalLine.length; index++) {
+                  line = chartInstance.options.horizontalLine[index];
+          
+                  if (!line.style) {
+                    style = "rgba(169,169,169, .6)";
+                  } else {
+                    style = line.style;
+                  }
+          
+                  if (line.y) {
+                    yValue = yScale.getPixelForValue(line.y);
+                  } else {
+                    yValue = 1500;
+                  }
+          
+                  ctx.lineWidth = 3;
+          
+                  if (yValue) {
+                    ctx.beginPath();
+                    ctx.moveTo(0, yValue);
+                    ctx.lineTo(canvas.width, yValue);
+                    ctx.strokeStyle = style;
+                    ctx.stroke();
+                  }
+          
+                  if (line.text) {
+                    ctx.fillStyle = style;
+                    ctx.fillText(line.text, 0, yValue + ctx.lineWidth);
+                  }
+                }
+                return;
+              };
+            }
+          };
+          Chart.pluginService.register(horizonalLinePlugin);
         
         const config = {
             type: 'line',
             data: data,
-            plugins: [GradientBgPlugin],
+            options: {
+                "horizontalLine": [{
+                  "y": 5000,
+                  "style": "rgba(255, 0, 0, .4)",
+                  "text": "max"
+                }, {
+                  "y": -5000,
+                  "style": "rgba(255, 0, 0, .4)",
+                  "text": "min"
+                }]
+              }
             };
 
         var ctx = document.getElementById('myChart').getContext('2d');
